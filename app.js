703ed3ad66939679dc9559e8489a8ccdd7d85696
krushndayshmookh <krushndayshmookh@gmail.com>
2017-05-29 22:30:46 +0530

    some trial and lots of error

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 
-app.use(express.static('www'))
+app.use(express.static('.'))
 /*app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,19 +14,32 @@ app.use(express.static('www'))
 });*/
 
 app.get('/', function (req, res) {
-	res.sendFile("./www/index.html")//, function (err, data) {
-		/*res.writeHead(200, {
-			'Content-Type': 'text/html'
-		});*/
-		//res.write(data);
-		//res.end();
-//	});
+	res.sendFile("./www/index.html") //, function (err, data) {
+	/*res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});*/
+	//res.write(data);
+	//res.end();
+	//	});
 });
 app.get('/trial', function (req, res) {
 	console.log("request");
 	console.log(req.query);
+	fs.readFile("data/users/users.json", function (err, data) {
+		users = JSON.parse(data);
+		console.log(users);
+		username = req.query.uname;
+		console.log(username)
+		if (req.query.uname in users) {
+			/*for (username in users) {
+				console.log(username.password);
+				console.log(username);
+			}*/ // ------ Implement this
+		}
+	});
+
 	//if (req.query) {
-	res.send("heelo");
+	res.send("hello");
 	//}
 });
 
@@ -49,5 +62,3 @@ app.listen(3000, function () {
 //app.use(function (req, res, next) {
 //  res.status(404).sendFile("www/pages/error404.html")
 //})
-
-
