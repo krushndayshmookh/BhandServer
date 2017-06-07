1ad0ca7a4883a50872b785272c79da705573ec5a
krushndayshmookh <krushndayshmookh@gmail.com>
2017-06-07 12:43:28 +0530

    added chat server

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ var ipaddress = config.ipaddress;
 var appport = config.appport;
 var dataport = config.dataport;
 var paperport = config.paperport;
+var chatport = config.chatport;
 
 
 
@@ -54,16 +55,16 @@ var paperport = config.paperport;
  */
 
 
-const app = express();
+var app = express();
 
 
 
 //app.use(express.static('www'))
 //app.use('/data', express.static('data'))
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 /*app.get('/', function (req, res) {
@@ -79,110 +80,110 @@ app.use(function (req, res, next) {
 
 
 app.get('/login', function (req, res) {
-	//console.log("request");
-	//console.log(req.query);
-	var username = req.query.uname;
-	//console.log(username);
-	var password = req.query.pword;
+    //console.log("request");
+    //console.log(req.query);
+    var username = req.query.uname;
+    //console.log(username);
+    var password = req.query.pword;
 
 
-	fs.readFile("data/users/users.json", function (err, data) {
-		var users = JSON.parse(data);
+    fs.readFile("data/users/users.json", function (err, data) {
+        var users = JSON.parse(data);
 
-		//console.log(users);
-		//console.log(JSON.parse(data));
-		//console.log(username)
-		if (username in users) {
-			//;
-			if (users[username].password == password) {
-				res.send("true");
-				//console.log(users[username].password);
-				console.log(username + " logged in.");
-			} else {
-				res.send("false");
-				console.log(username + " entered incorrect password.");
-			}
+        //console.log(users);
+        //console.log(JSON.parse(data));
+        //console.log(username)
+        if (username in users) {
+            //;
+            if (users[username].password == password) {
+                res.send("true");
+                //console.log(users[username].password);
+                console.log(username + " logged in.");
+            } else {
+                res.send("false");
+                console.log(username + " entered incorrect password.");
+            }
 
-		} else {
-			res.send("invalid");
-			console.log(username + " does not exist.");
-		}
-	});
+        } else {
+            res.send("invalid");
+            console.log(username + " does not exist.");
+        }
+    });
 });
 
 
 
 
 app.get('/userdata', function (req, res) {
-	//console.log("request");
-	//console.log(req.query);
-	var username = req.query.username;
-	//console.log(username);
+    //console.log("request");
+    //console.log(req.query);
+    var username = req.query.username;
+    //console.log(username);
 
-	fs.readFile(config.userlist, function (err0, userdata) {
-		var users = JSON.parse(userdata);
+    fs.readFile(config.userlist, function (err0, userdata) {
+        var users = JSON.parse(userdata);
 
-		//console.log(users);
-		//console.log(JSON.parse(data));
-		//console.log(username);
-		//console.log(users[username].type);
-		if (username in users) {
-			//console.log(users[username].type);
-			var userpath = "data/users/" + users[username].type + "/" + username + "/";
-			//console.log(userpath);
-			var profilepath = userpath + "profile.json";
-			//console.log(profilepath);
-			fs.readFile(profilepath, function (err1, profiledata) {
-				//console.log(profiledata);
+        //console.log(users);
+        //console.log(JSON.parse(data));
+        //console.log(username);
+        //console.log(users[username].type);
+        if (username in users) {
+            //console.log(users[username].type);
+            var userpath = "data/users/" + users[username].type + "/" + username + "/";
+            //console.log(userpath);
+            var profilepath = userpath + "profile.json";
+            //console.log(profilepath);
+            fs.readFile(profilepath, function (err1, profiledata) {
+                //console.log(profiledata);
 
-				var profile = JSON.parse(profiledata);
+                var profile = JSON.parse(profiledata);
 
-				//console.log(profile);
-				res.send(profile);
-				console.log("Profile of " + username + " sent.");
-
-
+                //console.log(profile);
+                res.send(profile);
+                console.log("Profile of " + username + " sent.");
 
 
-				/*fs.readFile(userpath+"user.png",function(err2,imgdata){
-					profile["user-img"] = imgdata;
-					//console.log(profile["user-img"]);
 
 
-				});*/
-			});
+                /*fs.readFile(userpath+"user.png",function(err2,imgdata){
+                	profile["user-img"] = imgdata;
+                	//console.log(profile["user-img"]);
 
-		}
-	});
+
+                });*/
+            });
+
+        }
+    });
 
 });
 
 app.get('/username-name', function (req, res) {
-	//console.log("request");
-	//console.log(req.query);
-	var username = req.query.user;
-	//console.log(username);
-	fs.readFile("data/users/users.json", function (err0, userdata) {
-		var users = JSON.parse(userdata);
-		//console.log(users);
-		//console.log(JSON.parse(data));
-		//console.log(username);
-		//console.log(users[username].type);
-		if (username in users) {
-			//console.log(users[username].type);
-			var userpath = "data/users/" + users[username].type + "/" + username + "/";
-			//console.log(userpath);
-			var profilepath = userpath + "profile.json";
-			//console.log(profilepath);
-			fs.readFile(profilepath, function (err1, profiledata) {
-				//console.log(profiledata);
-				var profile = JSON.parse(profiledata);
-				//console.log(profile);
-				res.send(profile.Name);
-				console.log("Name of " + username + " sent.");
-			});
-		}
-	});
+    //console.log("request");
+    //console.log(req.query);
+    var username = req.query.user;
+    //console.log(username);
+    fs.readFile("data/users/users.json", function (err0, userdata) {
+        var users = JSON.parse(userdata);
+        //console.log(users);
+        //console.log(JSON.parse(data));
+        //console.log(username);
+        //console.log(users[username].type);
+        if (username in users) {
+            //console.log(users[username].type);
+            var userpath = "data/users/" + users[username].type + "/" + username + "/";
+            //console.log(userpath);
+            var profilepath = userpath + "profile.json";
+            //console.log(profilepath);
+            fs.readFile(profilepath, function (err1, profiledata) {
+                //console.log(profiledata);
+                var profile = JSON.parse(profiledata);
+                //console.log(profile);
+                res.send(profile.Name);
+                console.log("Name of " + username + " sent.");
+            });
+        }
+    });
 
 });
 
@@ -190,17 +191,17 @@ app.get('/username-name', function (req, res) {
 
 
 app.get('/notifications', function (req, res) {
-	var type = req.query.type;
-	//  type can be "departmantal" or "general"
+    var type = req.query.type;
+    //  type can be "departmantal" or "general"
 
 
-	//console.log(type);
-	fs.readFile("data/notifications/" + type + ".json", function (err0, notificationdata) {
-		var notifications = JSON.parse(notificationdata);
-		//console.log(notifications);
-		res.send(notifications);
-		console.log(type + " notifications sent.");
-	});
+    //console.log(type);
+    fs.readFile("data/notifications/" + type + ".json", function (err0, notificationdata) {
+        var notifications = JSON.parse(notificationdata);
+        //console.log(notifications);
+        res.send(notifications);
+        console.log(type + " notifications sent.");
+    });
 
 });
 
@@ -209,53 +210,53 @@ app.get('/notifications', function (req, res) {
 
 
 app.get('/attendance', function (req, res) {
-	var username = req.query.username;
-	//  type can be "departmantal" or "general"
-	fs.readFile("data/users/users.json", function (err0, userdata) {
-		var users = JSON.parse(userdata);
-		//console.log(type);
-		var userpath = "data/users/" + users[username].type + "/" + username + "/";
-		//console.log(userpath);
-		var attendancepath = userpath + "attendance.json";
-		//console.log(profilepath);
-		fs.readFile(attendancepath, function (err1, attendancedata) {
-			//console.log(profiledata);
+    var username = req.query.username;
+    //  type can be "departmantal" or "general"
+    fs.readFile("data/users/users.json", function (err0, userdata) {
+        var users = JSON.parse(userdata);
+        //console.log(type);
+        var userpath = "data/users/" + users[username].type + "/" + username + "/";
+        //console.log(userpath);
+        var attendancepath = userpath + "attendance.json";
+        //console.log(profilepath);
+        fs.readFile(attendancepath, function (err1, attendancedata) {
+            //console.log(profiledata);
 
-			var attendance = JSON.parse(attendancedata);
+            var attendance = JSON.parse(attendancedata);
 
-			//console.log(profile);
-			res.send(attendance);
-			console.log("Attendance of " + username + " sent.");
+            //console.log(profile);
+            res.send(attendance);
+            console.log("Attendance of " + username + " sent.");
 
 
-		});
-	});
+        });
+    });
 
 });
 
 app.get('/academics', function (req, res) {
-	var username = req.query.username;
-	//  type can be "departmantal" or "general"
-	fs.readFile("data/users/users.json", function (err0, userdata) {
-		var users = JSON.parse(userdata);
-		//console.log(type);
-		var userpath = "data/users/" + users[username].type + "/" + username + "/";
-		//console.log(userpath);
-		var academicspath = userpath + "academics.json";
-		//console.log(profilepath);
-		fs.readFile(academicspath, function (err1, academicsdata) {
-			//console.log(profiledata);
+    var username = req.query.username;
+    //  type can be "departmantal" or "general"
+    fs.readFile("data/users/users.json", function (err0, userdata) {
+        var users = JSON.parse(userdata);
+        //console.log(type);
+        var userpath = "data/users/" + users[username].type + "/" + username + "/";
+        //console.log(userpath);
+        var academicspath = userpath + "academics.json";
+        //console.log(profilepath);
+        fs.readFile(academicspath, function (err1, academicsdata) {
+            //console.log(profiledata);
 
-			var academics = JSON.parse(academicsdata);
+            var academics = JSON.parse(academicsdata);
 
-			//console.log(profile);
-			res.send(academics);
-			console.log("Academics of " + username + " sent.");
+            //console.log(profile);
+            res.send(academics);
+            console.log("Academics of " + username + " sent.");
 
 
-		});
+        });
 
-	});
+    });
 });
 
 
@@ -273,8 +274,8 @@ app.get('/academics', function (req, res) {
 
 
 
-app.listen(appport,ipaddress, function () {
-	console.log('App listening on port 3000 on '+ipaddress+' !');
+app.listen(appport, ipaddress, function () {
+    console.log('App listening on port 3000 on ' + ipaddress + ' !');
 });
 
 
@@ -285,10 +286,10 @@ app.listen(appport,ipaddress, function () {
 
 // must be at last
 app.use(function (req, res, next) {
-	fs.readFile("www/error.html", function (err, data) {
-		res.status(404).write(data);
-		res.end();
-	});
+    fs.readFile("www/error.html", function (err, data) {
+        res.status(404).write(data);
+        res.end();
+    });
 });
 
 
@@ -317,7 +318,7 @@ app.use(function (req, res, next) {
  */
 
 
-const dataServer = express();
+var dataServer = express();
 
 
 
@@ -325,9 +326,9 @@ dataServer.use(express.static('www'));
 //app.use('/data', express.static('data'))
 
 dataServer.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 
@@ -338,8 +339,8 @@ dataServer.use(function (req, res, next) {
 
 
 
-dataServer.listen(dataport,ipaddress, function () {
-	console.log('Data Server listening on port 3030 on '+ipaddress+' !');
+dataServer.listen(dataport, ipaddress, function () {
+    console.log('Data Server listening on port 3030 on ' + ipaddress + ' !');
 });
 
 
@@ -350,10 +351,10 @@ dataServer.listen(dataport,ipaddress, function () {
 
 // must be at last
 dataServer.use(function (req, res, next) {
-	fs.readFile("www/error.html", function (err, data) {
-		res.status(404).write(data);
-		res.end();
-	});
+    fs.readFile("www/error.html", function (err, data) {
+        res.status(404).write(data);
+        res.end();
+    });
     //res.status(404).sendFile("error.html");
 });
 
@@ -377,7 +378,7 @@ dataServer.use(function (req, res, next) {
  */
 
 
-const paperServer = express();
+var paperServer = express();
 
 
 
@@ -386,9 +387,9 @@ paperServer.use(express.static('data/exam'));
 //app.use('/data', express.static('data'))
 
 paperServer.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 
@@ -398,46 +399,46 @@ paperServer.use(function (req, res, next) {
 
 
 paperServer.get('/papers', function (req, res) {
-	//console.log("request");
-	//console.log(req.query);
-	var requestpath = req.query.path;
-	var path = "data/exam" + requestpath;
+    //console.log("request");
+    //console.log(req.query);
+    var requestpath = req.query.path;
+    var path = "data/exam" + requestpath;
 
-	var sendableObject = {
-		"path": requestpath,
-		"dirs": [],
-		"files": []
-	};
+    var sendableObject = {
+        "path": requestpath,
+        "dirs": [],
+        "files": []
+    };
 
-	//console.log(username);
+    //console.log(username);
 
 
 
-	fs.readdir(path, function (err, pathcontents) {
-		//console.log(pathcontents);
-		for (item in pathcontents) {
-			//console.log(pathcontents[item]);
-			if (fs.statSync(path + "/" + pathcontents[item]).isDirectory()) {
+    fs.readdir(path, function (err, pathcontents) {
+        //console.log(pathcontents);
+        for (item in pathcontents) {
+            //console.log(pathcontents[item]);
+            if (fs.statSync(path + "/" + pathcontents[item]).isDirectory()) {
 
-				sendableObject.dirs.push(pathcontents[item]);
+                sendableObject.dirs.push(pathcontents[item]);
 
-				//console.log(sendableObject);
-			} else {
-				sendableObject.files.push(pathcontents[item]);
-				//console.log(sendableObject);
-			}
+                //console.log(sendableObject);
+            } else {
+                sendableObject.files.push(pathcontents[item]);
+                //console.log(sendableObject);
+            }
 
-		}
-		//console.log(sendableObject);
-		res.send(sendableObject);
-	});
+        }
+        //console.log(sendableObject);
+        res.send(sendableObject);
+    });
 
 });
 
 
 
-paperServer.listen(paperport,ipaddress, function () {
-	console.log('Paper Server listening on port 3040 on '+ipaddress+' !');
+paperServer.listen(paperport, ipaddress, function () {
+    console.log('Paper Server listening on port 3040 on ' + ipaddress + ' !');
 });
 
 
@@ -448,9 +449,99 @@ paperServer.listen(paperport,ipaddress, function () {
 
 // must be at last
 paperServer.use(function (req, res, next) {
-	fs.readFile("www/error.html", function (err, data) {
-		res.status(404).write(data);
-		res.end();
-	});
+    fs.readFile("www/error.html", function (err, data) {
+        res.status(404).write(data);
+        res.end();
+    });
 });
 
+
+
+
+
+
+
+
+/****************************
+ *                          *
+ *       CHAT SERVER        ****************************************************
+ *                          *
+ ****************************
+
+
+ * This part is used for forum chat will be accessible througn the app.
+ *
+ */
+
+
+// Setup basic express server
+var chat = express();
+var chatserver = require('http').createServer(chat);
+var io = require('socket.io')(chatserver);
+
+chatserver.listen(chatport, function () {
+    console.log('Server listening at port %d', chatport);
+});
+
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        if (addedUser) {
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});
--- config.js
+++ config.js
@@ -2,9 +2,10 @@ module.exports = {
 
     userlist: 'data/users/users.json',
     ipaddress: "127.0.0.1",
-    appport: 3000,
+    appport:  3000,
     dataport: 3030,
-    paperport:3040
+    paperport:3040,
+    chatport: 3050
 
 };
 
