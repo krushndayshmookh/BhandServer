252fa02918c09bd32dc016b9c611500832a786bc
Krushn Dayshmookh <krushn@dayshmookh.localdomain>
2017-11-18 17:31:41 +0530

    i dont know what i did

--- findinvalidcheck.js
+++ findinvalidcheck.js
@@ -4,7 +4,9 @@ var mongolaburi = "mongodb://clavi:clavidbpassword@ds117592.mlab.com:17592/clavi
 
 MongoClient.connect(mongolaburi, function (err, db) {
 
-    if (err) throw err;
+    if (err) {
+        throw err;
+    }
 
     console.log("Connected successfully to server");
 
@@ -13,7 +15,9 @@ MongoClient.connect(mongolaburi, function (err, db) {
     users.findOne({
         uid: "username"
     }, function (err1, data) {
-        if (err1) throw err1;
+        if (err1) {
+            throw err1;
+        }
 
         console.log(data);
 
