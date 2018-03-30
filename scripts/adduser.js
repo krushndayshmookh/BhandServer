6d0b7f2aff3dbe954ed5de42c95afef62cb58f59
krushndayshmookh <krushndayshmookh@gmail.com>
2018-03-31 00:53:44 +0530

    added users

--- scripts/adduser.js
+++ scripts/adduser.js
@@ -5,23 +5,73 @@
  */
 
 var MongoClient = require('mongodb').MongoClient;
-//var mongolaburi = "mongodb://clavi:clavidbpassword@ds117592.mlab.com:17592/clavi";
-var mongolaburi = "mongodb://127.0.0.1:27017";
+var mongolaburi = "mongodb://clavi:clavidbpassword@ds117592.mlab.com:17592/clavi";
+//var mongolaburi = "mongodb://127.0.0.1:27017";
 
 
-var users = [{
-        id: "2016ACSC0108072",
-        name: "Krushn Dayshmookh",
+var users = [
+    {
+        id: "2016ACSC0100015",
+        name: "Rahul Manusmare",
         type: "students",
-        email: "krushndayshmookh@gmail.com",
-        password: "2016ACSC0108072"
+        email: "manusmare_rahul.ghrcecs@gmail.com",
+        password: "2016ACSC0100015"
     },
     {
-        id: "2016ACSC0101067",
-        name: "Mrunalini Dudhagawali",
+        id: "2016ACSC0100016",
+        name: "Karan Bagle",
         type: "students",
-        email: "dudhagawali@gmail.com",
-        password: "2016ACSC0101067"
+        email: "bagle_karan.ghrcecs@gmail.com",
+        password: "2016ACSC0100016"
+    },
+    {
+        id: "2016ACSC0101010",
+        name: "Akash Thakre",
+        type: "students",
+        email: "thakre_akash.ghrcecs@gmail.com",
+        password: "2016ACSC0101010"
+    },
+    {
+        id: "2016ACSC0101011",
+        name: "Shefali Dayshmookh",
+        type: "students",
+        email: "Dayshmookh_shefali.ghrcecs@gmail.com",
+        password: "2016ACSC0101011"
+    },
+    {
+        id: "2016ACSC0101012",
+        name: "Vaibhav Pathak",
+        type: "students",
+        email: "pathak_vaibhav.ghrcecs@gmail.com",
+        password: "2016ACSC0101012"
+    },
+    {
+        id: "2016ACSC0101013",
+        name: "Vitika Jha",
+        type: "students",
+        email: "jha_vitika.ghrcecs@gmail.com",
+        password: "2016ACSC0101013"
+    },
+    {
+        id: "2016ACSC0101078",
+        name: "Sankul Anmadwar",
+        type: "students",
+        email: "anmadwar_sankul.ghrcecs@gmail.com",
+        password: "2016ACSC0101078"
+    },
+    {
+        id: "2016ACSC0111045",
+        name: "Piyush Joshi",
+        type: "students",
+        email: "joshi_piyush.ghrcecs@gmail.com",
+        password: "2016ACSC0111045"
+    },
+    {
+        id: "2016ACSC0117215",
+        name: "Savneet Chhatwal",
+        type: "students",
+        email: "chhatwal_savneet.ghrcecs@gmail.com",
+        password: "2016ACSC0117215"
     },
     {
         id: "samplestudent",
@@ -37,8 +87,6 @@ var users = [{
         email: "someteachermail@gmail.com",
         password: "sampleteacherpassword"
 	},
-
-
     {
         id: "octocat",
         name: "Master Octocat",
