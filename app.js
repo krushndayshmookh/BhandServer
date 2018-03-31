2f44e4984244d98784a6a657e09fb667a0b2a607
krushndayshmookh <krushndayshmookh@gmail.com>
2018-03-31 13:00:01 +0530

    s

--- app.js
+++ app.js
@@ -44,7 +44,11 @@ var mongolaburi = process.env.MONGOLAB_URI || "mongodb://clavi:clavidbpassword@d
 //var mongolaburi = "mongodb://127.0.0.1:27017";
 
 
-
+app.use(function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //next();
+});
 /****************************
  *                          *
  *         APP DATA         ****************************************************
@@ -61,11 +65,7 @@ var mongolaburi = process.env.MONGOLAB_URI || "mongodb://clavi:clavidbpassword@d
 
 
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 
 /*   --- this is user prototype that is saved in database
 var user = function (uid, uname, type, email, passwd) {
